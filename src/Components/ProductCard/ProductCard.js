019a96539ddf37d5384b2a5ea200f88483744e1c
Stetson Harding <stetson2001@msn.com>
2021-04-16 15:11:14 -0700

    Product_Cards Completed

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -1,30 +1,106 @@
-import React from "react"
+import React, { useState } from "react"
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from "@material-ui/core"
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import { IconButton } from '@material-ui/core';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Collapse from '@material-ui/core/Collapse';
+import Typography from '@material-ui/core/Typography';
+import clsx from 'clsx';
+import Button from '@material-ui/core/Button';
 
 
 
-const ProductCard = ({product}) =>{
-    return(
-      <div>
-          <Card>
-              <CardHeader
-              
-            action={
-                <IconButton>
-                     <MoreVertIcon></MoreVertIcon>
-                </IconButton>
-            }
 
-              />
-          </Card>
-      </div>   
-    )
+
+
+
+const ProductCard = ({ product }) => {
+  const [expanded, setExpanded] = useState(false);
+
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: 275,
+      margin: "0 auto"
+    },
+    header:{
+      textAlign: "center"
+    },
+    media: {
+      height: 0,
+      width: "100px",
+      paddingTop: '56.25%', // 16:9
+      margin: "0 auto",
+    },
+    button:{
+      color: "black",
+      fontWeight: 900
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(360deg)',
+    }
+  }));
+  const classes = useStyles()
+
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded)
+  }
+
+
+  return (
+    <Grid item xs={12} sm={6} md={6} lg={3} >
+      <Card className={classes.root} style={{ backgroundColor: "white" }}>
+        <CardHeader
+        className={classes.header}
+          title={product.name}
+          subheader="product"
+        />
+        <CardMedia
+          className={classes.media}
+          image={product.img} style={{ width: "135px" }} alt=""
+        />
+        <CardActions>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            {/* <ExpandMoreIcon /> */}
+            <Button className={classes.button}>Learn More</Button>
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>{product.name} -</Typography>
+            <Typography paragraph>
+              {product.about}
+          </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </Grid>
+
+
+
+
+
+
+  )
 }
 
 export default ProductCard
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,26 +1,40 @@
 import React from 'react'
+import {Grid}from "@material-ui/core"
+import { makeStyles } from '@material-ui/core/styles';
 
 import Hero from "../Components/Hero/Hero"
 
 import {ProductItems} from "../Components/ProductCard/ProductItems"
 import ProductCard from "../Components/ProductCard/ProductCard"
 
+// import Grid_Background from "../Assets/Card_Background.jpg"
+
+const useStyles = makeStyles((theme) => ({
+   Grid: {
+    
+   
+     
+   }
+ }));
+ 
+
 function Home() {
+
+
+    const classes = useStyles()
 
     return (
        <>
        <section>
-       <Hero />
+         <Hero />
        </section>
        
        <section>
-       <div className="products container">
-          
-            {ProductItems.map(product =>(
-               <ProductCard key={product.id} product={product} /> 
-            ))}
-          
-       </div>
+         <Grid className={classes.Grid}container spacing={4}>
+               {ProductItems.map(product =>(
+                  <ProductCard key={product.id} product={product} /> 
+               ))}
+         </Grid>
        </section>
        
        
